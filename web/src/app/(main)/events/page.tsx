556aa10b93f86b023a46f378cd3a4f9926f45875
Leozheng04 <leozheng1204@gmail.com>
2026-05-04 17:39:59 -0400

    Finished evens page layout

--- web/src/app/(main)/events/page.tsx
+++ web/src/app/(main)/events/page.tsx
@@ -1,3 +1,32 @@
 export default function EventsPage() {
-  return <div className="p-6">Events coming soon</div>;
+  const pills = [
+    "All Events",
+    "🎉 Social",
+    "🎓 Training",
+    "🎾 Fetch",
+    "🥾 Hiking",
+    "🏊 Swimming",
+  ];
+  return(
+    <>
+      <main className="min-h-screen bg-[#FDF6EE] p-8">
+        <div className="text-[#3D2C2C] text-2xl font-bold">Events</div>
+        <div className="text-[#A89279] mb-[20px]">6 paw-some events nearby</div>
+        {/* Filter pills */}
+        <div className="flex gap-2 overflow-x-auto">
+          {pills.map((pill, index) => {
+            return (
+              <button
+                key={index}
+                className="px-4 py-2 rounded-full border border-[#E8DDD0] bg-white text-sm"
+              >
+                {pill}
+              </button>
+            );
+          })}
+        </div>
+      </main> 
+    </>
+
+  )
 }
